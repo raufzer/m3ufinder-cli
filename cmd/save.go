355143fc1117b2b6e288fd62d8fe71b7cac672a8
raufzer <a_zerkhef@estin.dz>
2025-03-20 15:59:10 +0100

    feat: add collect command to extract IPTV links

--- cmd/save.go
+++ cmd/save.go
@@ -1,26 +1,27 @@
 package cmd
 
 import (
-    "fmt"
-    "m3ufinder-cli/internal/saver"
+	"fmt"
+	"m3ufinder-cli/internal/saver"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
-// saveCmd represents the save command
 var saveCmd = &cobra.Command{
-    Use:   "save [format]",
-    Short: "Exports extracted links in .m3u, .json, or .csv formats",
-    Args:  cobra.ExactArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-        format := args[0]
-        err := saver.SaveM3U(m3uLinks, format)
-        if err != nil {
-            fmt.Println("Error saving links:", err)
-        }
-    },
+	Use:   "save [format]",
+	Short: "Exports extracted links in .m3u, .json, or .csv formats",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		format := args[0]
+		err := saver.SaveM3U(m3uLinks, format)
+		if err != nil {
+			fmt.Println("Error saving links:", err)
+		} else {
+			fmt.Println("Links saved successfully.")
+		}
+	},
 }
 
 func init() {
-    rootCmd.AddCommand(saveCmd)
+	rootCmd.AddCommand(saveCmd)
 }
